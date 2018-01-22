229b9e95c3f51d2007babff1687091b400d8548b
Enkhbaatar Battulga <35048685+AlexBattulga@users.noreply.github.com>
2018-01-22 00:57:39 -0500

    updated js

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ var itsNight = true;
 /* undefined variable  */
 var college;
 
-console.log('My first number is: ' + firstNum);
-console.log('My second number is: ' + secondNum);
-console.log('My third number is: ' + thirdNum);
+console.log('My first number is: ', firstNum);
+console.log('My second number is: ', secondNum);
+console.log('My third number is: ', thirdNum);
 
-console.log('My name 1 is: ' + name1);
-console.log('My name 2 is: ' + name2);
-console.log('My name 3 is: ' + name3);
+console.log('My name 1 is: ', name1);
+console.log('My name 2 is: ', name2);
+console.log('My name 3 is: ', name3);
 
-console.log('Is it raining: ' + raining);
-console.log('Is it snowing: ' + snowing);
-console.log('Is it night: ' + itsNight);
+console.log('Is it raining: ', raining);
+console.log('Is it snowing: ', snowing);
+console.log('Is it night: ', itsNight);
