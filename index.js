a06291239e4f66fef22d273aa7e92a403ac5260a
Enkbaatar Battulga <ebar1126@gmail.com>
2018-01-22 00:27:18 -0500

    adding simple JS practice

---
+++ index.js
@@ -0,0 +1,26 @@
+/* numbers */
+var firstNum = 10;
+var secondNum = -20;
+var thirdNum = 40;
+/* Strings  */
+var name1 = 'Alex';
+var name2 = 'Tina';
+var name3 = 'Totoro';
+/* Booleans  */
+var raining = true;
+var snowing = false;
+var itsNight = true;
+/* undefined variable  */
+var college;
+
+console.log('My first number is: ' + firstNum);
+console.log('My second number is: ' + secondNum);
+console.log('My third number is: ' + thirdNum);
+
+console.log('My name 1 is: ' + name1);
+console.log('My name 2 is: ' + name2);
+console.log('My name 3 is: ' + name3);
+
+console.log('Is it raining: ' + raining);
+console.log('Is it snowing: ' + snowing);
+console.log('Is it night: ' + itsNight);
